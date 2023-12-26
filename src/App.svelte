0c981697ca7f5d2f0e5b84350e9f8696bf99281c
Tife <boluwatife1904@gmail.com>
2023-12-26 16:50:48 +0100

    added preloader

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
   import twitter from "./assets/twitter.svg";
   import instagram from "./assets/instagram.svg";
   import circle from "./assets/circle.png";
-  import rect from "./assets/Middle-Rectangle.svg"
+  import rect from "./assets/Middle-Rectangle.svg";
   import vectors from "./assets/vector group.jpg";
   import { ScrollTrigger } from "gsap/ScrollTrigger";
   import { Rive } from "@rive-app/canvas";
@@ -16,6 +16,7 @@
   let ballClick;
   let ballClick2;
   let fireRocket;
+  let firePreloader = function () {};
   let inputEl;
   let subscribeBtn;
   let isBtnActive = false;
@@ -44,6 +45,48 @@
     // const heading = document.querySelector('.heading')
     let headingChars = new SplitType(".heading", { types: "words" });
 
+    window.addEventListener("load", () => {
+      const loader = new Rive({
+        src: "../loader.riv",
+        // Or the path to a public Rive asset
+        // src: '/public/example.riv',
+        // @ts-ignore
+        canvas: document.getElementById("loader-canvas"),
+        autoplay: true,
+        stateMachines: "proLign loader",
+        onLoad: () => {
+          loader.resizeDrawingSurfaceToCanvas();
+          // @ts-ignore
+          const inputs = loader.stateMachineInputs("proLign loader");
+          setTimeout(() => {
+            inputs[0].fire();
+          }, 3000);
+
+          setTimeout(() => {
+            loader.stop();
+          }, 5500);
+          console.log("hasLoaded");
+        },
+        onStop: () => {
+          console.log("hi there");
+          gsap.to(".loader-overlay", {
+            height: 0,
+            duration: 1,
+            ease: "bounce.out",
+            onStart: () => {
+              document.querySelector("#loader-canvas").classList.add("active");
+            },
+
+            onComplete: () => {
+              AnimatePage();
+            },
+          });
+        },
+      });
+    });
+  });
+
+  function AnimatePage() {
     function rollCircle() {
       gsap.fromTo(
         ".circle",
@@ -63,59 +106,112 @@
 
     gsap
       .timeline()
-      .from(".logo-box", {
-        y: -200,
-        opacity: 0,
-      })
-      .from(heading.querySelectorAll(".word"), {
-        y: 100,
-        opacity: 0,
-        stagger: 0.05,
-        duration: 0.4,
-        ease: "power1.inOut",
-        onComplete: () => {
-          fireRocket();
-          rollCircle();
+      .fromTo(
+        ".logo-box",
+        {
+          y: -200,
+          opacity: 0,
         },
-      })
-      .from(".follow-btn", {
-        y: 200,
-        opacity: 0,
-      })
-      .from(".social-icon", {
-        y: 100,
-        stagger: 0.3,
-        opacity: 0,
-      });
-
-    gsap.from(".input-box", {
-      y: 200,
-      x: "0%",
-      stagger: 0.3,
-      opacity: 0,
-      scrollTrigger: {
-        trigger: ".input-box-container",
-        // markers: true,
-        start: "bottom 83%",
-      },
-    });
-  });
+        {
+          opacity: 1,
+          y: 0,
+        }
+      )
+      .fromTo(
+        heading.querySelectorAll(".word"),
+        {
+          y: 100,
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          y: 0,
+          stagger: 0.05,
+          duration: 0.4,
+          ease: "power1.inOut",
+          onComplete: () => {
+            fireRocket();
+            rollCircle();
+          },
+        }
+      )
+      .fromTo(
+        ".follow-btn",
+        {
+          y: 200,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+        }
+      )
+      .fromTo(
+        ".social-icon",
+        {
+          y: 100,
+          stagger: 0.3,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+        }
+      )
+      .fromTo(
+        ".input-box",
+        {
+          y: 200,
+          x: "0%",
+          opacity: 0,
+          // scrollTrigger: {
+          //   trigger: ".input-box-container",
+          //   markers: true,
+          //   start: "bottom 83%",
+          // },
+        },
+        {
+          opacity: 1,
+          y: 0,
+          stagger: 0.3,
+        }
+      );
+  }
 </script>
 
 <main class="relative">
+  <!-- Container for the loader -->
+  <section
+    class="loader-overlay fixed w-full h-full left-0 top-0 bg-primOrange z-40 flex flex-col justify-center items-center"
+  >
+    <canvas
+      class="max-w-[500px] -500:max-w-[300px]"
+      id="loader-canvas"
+      height="500"
+      width="500"
+    ></canvas>
+
+    <!-- <button on:click={RevealPage}>FInished</button> -->
+  </section>
+
   <img src={vectors} class="absolute top-0 left-0 w-full h-full" alt="" />
+
   <!-- Container for logo and text -->
-  <div class="logo-box flex flex-col justify-center items-center">
+  <div class="logo-box opacity-0 flex flex-col justify-center items-center">
     <img src={logo} class="w-[80px]" alt="" />
   </div>
   <div
     class="w-full flex flex-col justify-center items-center pb-10 overflow-hidden relative"
   >
-  <!-- Rectangle vector that kinda fades in from the heading -->
-  <img class="h-[60%] w-[90%] -850:w-[98%] -850:h-[80%] left-[50%] translate-x-[-50%] absolute top-0" src={rect} alt="">
+    <!-- Rectangle vector that kinda fades in from the heading -->
+    <img
+      class="h-[60%] w-[90%] -850:w-[98%] -850:h-[80%] left-[50%] translate-x-[-50%] absolute top-0"
+      src={rect}
+      alt=""
+    />
     <h1
       bind:this={heading}
-      class="z-20 relative font-cascadiaMono heading text-4xl -950:text-3xl -500:text-2xl text-center max-w-[700px] font-semibold"
+      class="z-20 heading relative font-cascadiaMono heading text-4xl -950:text-3xl -500:text-2xl text-center max-w-[700px] font-normal"
     >
       Unlock productivity, elevate projects, and turbocharge team collaboration
 
@@ -136,15 +232,27 @@
     <!-- Container for button and social icons -->
     <div class="w-full flex flex-col gap-16 items-center overflow-hidden">
       <button
-        class="follow-btn font-franklinGoth bg-black border-black border-[1px] p-3 px-6 rounded-md text-white hover:bg-white hover:text-primOrange hover:border-[1px] hover:border-primOrange"
+        class="follow-btn opacity-0 font-franklinGoth bg-black border-black border-[1px] p-3 px-6 rounded-md text-white hover:bg-white hover:text-primOrange hover:border-[1px] hover:border-primOrange"
         >Follow us</button
       >
 
       <!--container for social icons -->
       <div class="flex gap-4 items-center justify-center">
-        <img class="social-icon cursor-pointer" src={linkedIn} alt="" />
-        <img class="social-icon cursor-pointer" src={twitter} alt="" />
-        <img class="social-icon cursor-pointer" src={instagram} alt="" />
+        <img
+          class="social-icon opacity-0 cursor-pointer"
+          src={linkedIn}
+          alt=""
+        />
+        <img
+          class="social-icon opacity-0 cursor-pointer"
+          src={twitter}
+          alt=""
+        />
+        <img
+          class="social-icon opacity-0 cursor-pointer"
+          src={instagram}
+          alt=""
+        />
       </div>
     </div>
 
@@ -195,7 +303,7 @@
       </div>
     </div>
 
-    <p class="mx-auto font-cascadiaMono font-semibold">
+    <p class="mx-auto font-cascadiaMono font-normal">
       ProLign is coming soon !
     </p>
   </div>
